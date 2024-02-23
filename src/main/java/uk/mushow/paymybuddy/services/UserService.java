e5cbcf0b80838d85b108f1e584952ffe556c0568
Mushow <antoine@mushow.uk>
2024-02-23 05:17:13 +0100

    Add createUser

---
+++ src/main/java/uk/mushow/paymybuddy/services/UserService.java
@@ -0,0 +1,32 @@
+package uk.mushow.paymybuddy.services;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import uk.mushow.paymybuddy.exceptions.EmailAlreadyInUseException;
+import uk.mushow.paymybuddy.models.User;
+import uk.mushow.paymybuddy.repositories.UserRepository;
+import uk.mushow.paymybuddy.services.utils.ArgonUtil;
+
+@Service
+@Log4j2
+@Transactional(propagation = Propagation.REQUIRES_NEW)
+public class UserService implements IUserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User createUser(User user) {
+        if (userRepository.existsByEmail(user.getEmail())) {
+            String invalidEmailMessage = "The email is already in use.";
+            log.error(invalidEmailMessage);
+            throw new EmailAlreadyInUseException(invalidEmailMessage);
+        }
+        String hashedPassword = ArgonUtil.hashPassword(user.getPassword());
+        user.setPassword(hashedPassword);
+        return userRepository.save(user);
+    }
+
+}
