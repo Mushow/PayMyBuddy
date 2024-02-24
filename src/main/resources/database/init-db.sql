52d764a26f8ff53d1a545e725daf00eb66231e89
Mushow <antoine@mushow.uk>
2024-02-24 13:07:13 +0100

    Change DB

--- src/main/resources/database/init-db.sql
+++ src/main/resources/database/init-db.sql
@@ -1,65 +1,37 @@
-drop database if exists bank;
+DROP DATABASE IF EXISTS PayMyBuddyDB;
 
-create database bank;
-use bank;
+CREATE DATABASE PayMyBuddyDB;
+USE PayMyBuddyDB;
 
-CREATE TABLE users (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    pseudo VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    balance DECIMAL DEFAULT 0.0
+CREATE TABLE `User` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `username` VARCHAR(20) UNIQUE NOT NULL,
+    `email` VARCHAR(50) UNIQUE NOT NULL,
+    `password` VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE user_friendships (
-    user_id BIGINT,
-    friend_id BIGINT,
-    PRIMARY KEY (user_id, friend_id),
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-    FOREIGN KEY (friend_id) REFERENCES users(id) ON DELETE CASCADE
+CREATE TABLE `Wallet` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `userId` INT NOT NULL,
+    `balance` BIGINT NOT NULL DEFAULT 0,
+    FOREIGN KEY (`userId`) REFERENCES `User` (`id`) ON DELETE CASCADE
 );
 
-CREATE TABLE user_wallet (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    account_number VARCHAR(255) NOT NULL UNIQUE,
-    balance DECIMAL NOT NULL DEFAULT 0.0,
-    user_id BIGINT,
-    FOREIGN KEY (user_id) REFERENCES users(id)
+CREATE TABLE `Transaction` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `issuerWalletId` INT NOT NULL,
+    `receiverWalletId` INT NOT NULL,
+    `timestamp` DATETIME DEFAULT CURRENT_TIMESTAMP,
+    `amount` BIGINT NOT NULL,
+    `description` VARCHAR(255),
+    FOREIGN KEY (`issuerWalletId`) REFERENCES `Wallet` (`id`) ON DELETE CASCADE,
+    FOREIGN KEY (`receiverWalletId`) REFERENCES `Wallet` (`id`) ON DELETE CASCADE
 );
 
-CREATE TABLE transactions (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    sender_account_number VARCHAR(255) NOT NULL,
-    receiver_account_number VARCHAR(255) NOT NULL,
-    description TEXT,
-    amount DECIMAL NOT NULL,
-    transaction_fee DECIMAL NOT NULL DEFAULT 0.0,
-    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
-    wallet_id BIGINT,
-    FOREIGN KEY (wallet_id) REFERENCES user_wallet(id)
+CREATE TABLE `Friends` (
+    `userId` INT NOT NULL,
+    `friendId` INT NOT NULL,
+    PRIMARY KEY (`userId`, `friendId`),
+    FOREIGN KEY (`userId`) REFERENCES `User` (`id`) ON DELETE CASCADE,
+    FOREIGN KEY (`friendId`) REFERENCES `User` (`id`) ON DELETE CASCADE
 );
-
-INSERT INTO users (pseudo, email, password, balance) VALUES
-('Jane', 'jane@example.com', 'password123', 5000.0),
-('Mark', 'mark@example.com', 'password123', 3000.0),
-('John', 'john@example.com', 'password123', 2000.0),
-('Ken', 'ken@example.com', 'password123', 4000.0);
-
--- Jane est amie avec Mark et John
-INSERT INTO user_friendships (user_id, friend_id) VALUES
-(1, 2),
-(1, 3);
-
--- Mark est ami avec Ken
-INSERT INTO user_friendships (user_id, friend_id) VALUES
-(2, 4);
-
--- John est ami avec Ken
-INSERT INTO user_friendships (user_id, friend_id) VALUES
-(3, 4);
-
-INSERT INTO user_wallet (account_number, balance, user_id) VALUES
-('ACC1001', 5000.0, 1),
-('ACC1002', 3000.0, 2),
-('ACC1003', 2000.0, 3),
-('ACC1004', 4000.0, 4);
