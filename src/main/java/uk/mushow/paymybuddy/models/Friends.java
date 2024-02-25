fb677404e8a084fb70556809c9c12fcc5c975299
Mushow <antoine@mushow.uk>
2024-02-25 16:05:43 +0100

    Added Friends Model

---
+++ src/main/java/uk/mushow/paymybuddy/models/Friends.java
@@ -0,0 +1,27 @@
+package uk.mushow.paymybuddy.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Friends")
+public class Friends {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @Column(nullable = false)
+    private Long friendId;
+
+}
