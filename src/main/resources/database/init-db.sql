4b0b9b1902597ccef395a4e1fac4bd405a055b0f
Mushow <antoine@mushow.uk>
2024-02-25 20:47:13 +0100

    Changed columns names to match JPA Hibernate naming convention

--- src/main/resources/database/init-db.sql
+++ src/main/resources/database/init-db.sql
@@ -12,27 +12,26 @@ CREATE TABLE `User` (
 
 CREATE TABLE `Wallet` (
     `id` BIGINT AUTO_INCREMENT PRIMARY KEY,
-    `userId` BIGINT NOT NULL,
+    `user_id` BIGINT NOT NULL,
     `balance` BIGINT NOT NULL DEFAULT 0,
-    FOREIGN KEY (`userId`) REFERENCES `User` (`id`) ON DELETE CASCADE
+    FOREIGN KEY (`user_id`) REFERENCES `User` (`id`) ON DELETE CASCADE
 );
 
 CREATE TABLE `Transaction` (
     `id` BIGINT AUTO_INCREMENT PRIMARY KEY,
-    `issuerWalletId` BIGINT NOT NULL,
-    `receiverWalletId` BIGINT NOT NULL,
+    `issuer_wallet_id` BIGINT NOT NULL,
+    `receiver_wallet_id` BIGINT NOT NULL,
     `timestamp` DATETIME DEFAULT CURRENT_TIMESTAMP,
     `amount` BIGINT NOT NULL,
-    'transactionFee' DOUBLE NOT NULL DEFAULT 0.05,
     `description` VARCHAR(255),
-    FOREIGN KEY (`issuerWalletId`) REFERENCES `Wallet` (`id`) ON DELETE CASCADE,
-    FOREIGN KEY (`receiverWalletId`) REFERENCES `Wallet` (`id`) ON DELETE CASCADE
+    FOREIGN KEY (`issuer_wallet_id`) REFERENCES `Wallet` (`id`) ON DELETE CASCADE,
+    FOREIGN KEY (`receiver_wallet_id`) REFERENCES `Wallet` (`id`) ON DELETE CASCADE
 );
 
 CREATE TABLE `Friends` (
-    `userId` BIGINT NOT NULL,
-    `friendId` BIGINT NOT NULL,
-    PRIMARY KEY (`userId`, `friendId`),
-    FOREIGN KEY (`userId`) REFERENCES `User` (`id`) ON DELETE CASCADE,
-    FOREIGN KEY (`friendId`) REFERENCES `User` (`id`) ON DELETE CASCADE
+    `user_id` BIGINT NOT NULL,
+    `friend_id` BIGINT NOT NULL,
+    PRIMARY KEY (`user_id`, `friend_id`),
+    FOREIGN KEY (`user_id`) REFERENCES `User` (`id`) ON DELETE CASCADE,
+    FOREIGN KEY (`friend_id`) REFERENCES `User` (`id`) ON DELETE CASCADE
 );
