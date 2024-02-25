bb0b51bd7e07f5fed64c96823ac41e5a806203c7
Mushow <antoine@mushow.uk>
2024-02-25 20:46:51 +0100

    Add login using UserDetailsService

---
+++ src/main/java/uk/mushow/paymybuddy/services/CustomUserDetailsService.java
@@ -0,0 +1,39 @@
+package uk.mushow.paymybuddy.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import uk.mushow.paymybuddy.models.User;
+import uk.mushow.paymybuddy.repositories.UserRepository;
+
+import java.util.Collections;
+import java.util.List;
+
+@Service
+@Transactional
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + username));
+
+
+        List<GrantedAuthority> authorities = Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                authorities
+        );
+    }
+
+}
