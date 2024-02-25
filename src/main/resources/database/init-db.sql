e917a780bc0fb14a2569681719fd7cad405399d4
Mushow <antoine@mushow.uk>
2024-02-25 16:07:45 +0100

    Changed DB to use Long

--- src/main/resources/database/init-db.sql
+++ src/main/resources/database/init-db.sql
@@ -4,23 +4,23 @@ CREATE DATABASE PayMyBuddyDB;
 USE PayMyBuddyDB;
 
 CREATE TABLE `User` (
-    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `id` BIGINT AUTO_INCREMENT PRIMARY KEY,
     `username` VARCHAR(20) UNIQUE NOT NULL,
     `email` VARCHAR(50) UNIQUE NOT NULL,
     `password` VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE `Wallet` (
-    `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `userId` INT NOT NULL,
+    `id` BIGINT AUTO_INCREMENT PRIMARY KEY,
+    `userId` BIGINT NOT NULL,
     `balance` BIGINT NOT NULL DEFAULT 0,
     FOREIGN KEY (`userId`) REFERENCES `User` (`id`) ON DELETE CASCADE
 );
 
 CREATE TABLE `Transaction` (
-    `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `issuerWalletId` INT NOT NULL,
-    `receiverWalletId` INT NOT NULL,
+    `id` BIGINT AUTO_INCREMENT PRIMARY KEY,
+    `issuerWalletId` BIGINT NOT NULL,
+    `receiverWalletId` BIGINT NOT NULL,
     `timestamp` DATETIME DEFAULT CURRENT_TIMESTAMP,
     `amount` BIGINT NOT NULL,
     'transactionFee' DOUBLE NOT NULL DEFAULT 0.05,
@@ -30,8 +30,8 @@ CREATE TABLE `Transaction` (
 );
 
 CREATE TABLE `Friends` (
-    `userId` INT NOT NULL,
-    `friendId` INT NOT NULL,
+    `userId` BIGINT NOT NULL,
+    `friendId` BIGINT NOT NULL,
     PRIMARY KEY (`userId`, `friendId`),
     FOREIGN KEY (`userId`) REFERENCES `User` (`id`) ON DELETE CASCADE,
     FOREIGN KEY (`friendId`) REFERENCES `User` (`id`) ON DELETE CASCADE
