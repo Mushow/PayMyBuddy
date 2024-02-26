616259a4fd5d0704b221178bd545e60d7ab2211d
Mushow <antoine@mushow.uk>
2024-02-26 23:35:18 +0100

    Add home controller and try to get wallet info

--- src/main/java/uk/mushow/paymybuddy/controllers/HomeController.java
+++ src/main/java/uk/mushow/paymybuddy/controllers/HomeController.java
@@ -1,13 +1,12 @@
 package uk.mushow.paymybuddy.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import uk.mushow.paymybuddy.services.CustomUserDetailsService;
+import uk.mushow.paymybuddy.userdetails.CustomUserDetails;
+import uk.mushow.paymybuddy.userdetails.CustomUserDetailsService;
 import uk.mushow.paymybuddy.services.WalletService;
 
 @Controller
@@ -17,10 +16,13 @@ public class HomeController {
     private WalletService walletService;
 
     @GetMapping("/home")
-    public String getHomePage(Model model, @AuthenticationPrincipal CustomUserDetailsService userDetails) {
-        //Long userId = getUserIdFromUserDetails(userDetails);
-        //model.addAttribute("balance", walletService.getBalance(userId));
+    public String getHomePage(Model model, @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        Long userId = customUserDetails.getUserId();
+        System.out.println("userId = " + userId);
+        model.addAttribute("balance", walletService.getBalance(userId));
+        System.out.println(walletService.getBalance(userId));
         return "home";
     }
+
 
 }
