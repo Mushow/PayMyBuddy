e5f1325691552c3c6ae28cae0fdb9d24826a46f4
Mushow <antoine@mushow.uk>
2024-02-26 23:02:47 +0100

    Add getBalance()

---
+++ src/main/java/uk/mushow/paymybuddy/services/WalletService.java
@@ -0,0 +1,23 @@
+package uk.mushow.paymybuddy.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import uk.mushow.paymybuddy.models.Wallet;
+import uk.mushow.paymybuddy.repositories.WalletRepository;
+
+import java.math.BigDecimal;
+
+@Service
+public class WalletService {
+
+    @Autowired
+    private WalletRepository walletRepository;
+
+    public BigDecimal getBalance(Long userId) {
+        return walletRepository.findByUserId(userId)
+                .map(Wallet::getBalance)
+                .orElse(BigDecimal.ZERO);
+    }
+
+}
+
