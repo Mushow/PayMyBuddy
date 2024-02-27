2e8ece68e560774a8d2d02246d7ad6dacc0b118f
Mushow <antoine@mushow.uk>
2024-02-27 05:51:42 +0100

    Add list of transactions and changed userId to User

--- src/main/java/uk/mushow/paymybuddy/models/Wallet.java
+++ src/main/java/uk/mushow/paymybuddy/models/Wallet.java
@@ -6,23 +6,35 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import java.math.BigDecimal;
+import java.util.List;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "Wallet")
+@Table(name = "wallet")
 public class Wallet {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
-    private Long userId;
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 
     @Column(precision = 19, scale = 2, nullable = false)
     private BigDecimal balance;
+
+    @OneToMany(mappedBy = "issuerWalletId")
+    private List<Transaction> transactions;
+
+    /*
+
+    + John : 1000$ (received)
+    - Amida : 1000$ (sent)
+
+     */
 
 }
