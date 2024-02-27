489f0551c85f9425a6502cb95210e3f429c5b43e
Mushow <antoine@mushow.uk>
2024-02-27 05:50:56 +0100

    Add existsByUsername to avoid error generation

--- src/main/java/uk/mushow/paymybuddy/repositories/UserRepository.java
+++ src/main/java/uk/mushow/paymybuddy/repositories/UserRepository.java
@@ -15,4 +15,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 
     boolean existsByEmail(String email);
+
+    boolean existsByUsername(String username);
 }
