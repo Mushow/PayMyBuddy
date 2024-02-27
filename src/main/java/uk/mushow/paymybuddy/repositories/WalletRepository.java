00251c0b02659b338842edbef9e3f3da8b2838f7
Mushow <antoine@mushow.uk>
2024-02-27 05:52:09 +0100

    Renamed method

--- src/main/java/uk/mushow/paymybuddy/repositories/WalletRepository.java
+++ src/main/java/uk/mushow/paymybuddy/repositories/WalletRepository.java
@@ -1,7 +1,6 @@
 package uk.mushow.paymybuddy.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import uk.mushow.paymybuddy.models.Wallet;
@@ -11,8 +10,6 @@ import java.util.Optional;
 @Repository
 public interface WalletRepository extends JpaRepository<Wallet, Long> {
 
-    @Query("SELECT w FROM Wallet w WHERE w.userId = :userId")
     Optional<Wallet> findByUserId(@Param("userId") Long userId);
-
 
 }
