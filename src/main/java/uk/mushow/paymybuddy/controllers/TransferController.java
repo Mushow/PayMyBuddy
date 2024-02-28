a860c5e6c385a97dea7f99ed452cf1fa063a43f0
Mushow <antoine@mushow.uk>
2024-02-28 04:25:39 +0100

    Add friends to view TH

--- src/main/java/uk/mushow/paymybuddy/controllers/TransferController.java
+++ src/main/java/uk/mushow/paymybuddy/controllers/TransferController.java
@@ -7,10 +7,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import uk.mushow.paymybuddy.models.User;
+import uk.mushow.paymybuddy.services.UserService;
 import uk.mushow.paymybuddy.services.WalletService;
 import uk.mushow.paymybuddy.userdetails.CustomUserDetails;
 
 import java.math.BigDecimal;
+import java.util.Set;
 
 @Controller
 public class TransferController {
@@ -18,16 +21,21 @@ public class TransferController {
     @Autowired
     private WalletService walletService;
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/transfer")
     public String showTransfer(Model model, @AuthenticationPrincipal CustomUserDetails customUserDetails) {
         BigDecimal availableFunds = walletService.getBalance(customUserDetails.getUserId());
+        Set<User> friends = userService.getFriends(customUserDetails.getUserId());
+        model.addAttribute("friends", friends);
         model.addAttribute("availableFunds", availableFunds);
         return "transfer";
     }
 
     @PostMapping("/transfer")
-    public String transfer(RedirectAttributes redirectAttributes) {
-        redirectAttributes.addFlashAttribute("error", "Email does not exist in the database.");
+    public String transfer(RedirectAttributes redirectAttributes, @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        redirectAttributes.addFlashAttribute("success", "Transfer completed!.");
         return "redirect:/transfer";
     }
 
