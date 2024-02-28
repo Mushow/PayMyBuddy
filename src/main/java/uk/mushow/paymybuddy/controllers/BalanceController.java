fdf598ae598f94fcac8e2c08494bddd8af995371
Mushow <antoine@mushow.uk>
2024-02-28 17:49:53 +0100

    Create transactions for top-ups and withdrawals

--- src/main/java/uk/mushow/paymybuddy/controllers/BalanceController.java
+++ src/main/java/uk/mushow/paymybuddy/controllers/BalanceController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import uk.mushow.paymybuddy.services.TransactionService;
 import uk.mushow.paymybuddy.services.WalletService;
 import uk.mushow.paymybuddy.userdetails.CustomUserDetails;
 
@@ -17,11 +18,17 @@ public class BalanceController {
     @Autowired
     private WalletService walletService;
 
+    @Autowired
+    private TransactionService transactionService;
+
     @PostMapping("/topup")
-    public String topUp(@AuthenticationPrincipal CustomUserDetails userDetails, RedirectAttributes redirectAttributes, @RequestParam("amount") BigDecimal amount) {
+    public String topUp(@AuthenticationPrincipal CustomUserDetails userDetails,
+                        RedirectAttributes redirectAttributes,
+                        @RequestParam("amount") BigDecimal amount) {
         Long userId = userDetails.getUserId();
         try {
             walletService.topUpBalance(userId, amount);
+            transactionService.topUp(userId, amount, "Add from bank!");
             redirectAttributes.addFlashAttribute("message", "Balance topped up successfully!");
         } catch (Exception e) {
             redirectAttributes.addFlashAttribute("error", e.getMessage());
@@ -30,10 +37,13 @@ public class BalanceController {
     }
 
     @PostMapping("/withdraw")
-    public String withdraw(@AuthenticationPrincipal CustomUserDetails userDetails, RedirectAttributes redirectAttributes, @RequestParam("amount") BigDecimal amount) {
+    public String withdraw(@AuthenticationPrincipal CustomUserDetails userDetails,
+                           RedirectAttributes redirectAttributes,
+                           @RequestParam("amount") BigDecimal amount) {
         Long userId = userDetails.getUserId();
         try {
             walletService.withdrawFromBalance(userId, amount);
+            transactionService.withdraw(userId, amount, "Withdraw to bank!");
             redirectAttributes.addFlashAttribute("message", "Balance withdrawn successfully!");
         } catch (Exception e) {
             redirectAttributes.addFlashAttribute("error", e.getMessage());
