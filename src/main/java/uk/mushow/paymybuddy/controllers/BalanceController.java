adba882435b0ada0d47763240d108c0eac2e69cb
Mushow <antoine@mushow.uk>
2024-02-28 14:19:04 +0100

    Add topup & withdraw functionalities

---
+++ src/main/java/uk/mushow/paymybuddy/controllers/BalanceController.java
@@ -0,0 +1,43 @@
+package uk.mushow.paymybuddy.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import uk.mushow.paymybuddy.services.WalletService;
+import uk.mushow.paymybuddy.userdetails.CustomUserDetails;
+
+import java.math.BigDecimal;
+
+@Controller
+public class BalanceController {
+
+    @Autowired
+    private WalletService walletService;
+
+    @PostMapping("/topup")
+    public String topUp(@AuthenticationPrincipal CustomUserDetails userDetails, RedirectAttributes redirectAttributes, @RequestParam("amount") BigDecimal amount) {
+        Long userId = userDetails.getUserId();
+        try {
+            walletService.topUpBalance(userId, amount);
+            redirectAttributes.addFlashAttribute("message", "Balance topped up successfully!");
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("error", e.getMessage());
+        }
+        return "redirect:/home";
+    }
+
+    @PostMapping("/withdraw")
+    public String withdraw(@AuthenticationPrincipal CustomUserDetails userDetails, RedirectAttributes redirectAttributes, @RequestParam("amount") BigDecimal amount) {
+        Long userId = userDetails.getUserId();
+        try {
+            walletService.withdrawFromBalance(userId, amount);
+            redirectAttributes.addFlashAttribute("message", "Balance withdrawn successfully!");
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("error", e.getMessage());
+        }
+        return "redirect:/home";
+    }
+}
