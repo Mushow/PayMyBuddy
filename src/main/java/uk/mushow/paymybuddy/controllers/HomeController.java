8eaaa0a1774d120604ae8220bc5684a0d15933df
Mushow <antoine@mushow.uk>
2024-02-28 17:53:51 +0100

    Add data to model

--- src/main/java/uk/mushow/paymybuddy/controllers/HomeController.java
+++ src/main/java/uk/mushow/paymybuddy/controllers/HomeController.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import uk.mushow.paymybuddy.services.TransactionService;
 import uk.mushow.paymybuddy.services.UserService;
 import uk.mushow.paymybuddy.services.WalletService;
 import uk.mushow.paymybuddy.userdetails.CustomUserDetails;
@@ -18,11 +19,16 @@ public class HomeController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private TransactionService transactionService;
+
     @GetMapping("/home")
     public String getHomePage(Model model, @AuthenticationPrincipal CustomUserDetails customUserDetails) {
         Long userId = customUserDetails.getUserId();
+        model.addAttribute("user", userService.getUserById(userId));
         model.addAttribute("balance", walletService.getBalance(userId));
         model.addAttribute("friends", userService.getFriends(userId));
+        model.addAttribute("transactions", transactionService.getTransactionsDTO(userId));
         return "home";
     }
 
