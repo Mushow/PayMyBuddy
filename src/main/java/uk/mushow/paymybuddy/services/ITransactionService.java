3162f43b2d4e3f5617c3df37e1a2fd819b651777
Mushow <antoine@mushow.uk>
2024-02-28 17:53:41 +0100

    Add ITransactionService methods

--- src/main/java/uk/mushow/paymybuddy/services/ITransactionService.java
+++ src/main/java/uk/mushow/paymybuddy/services/ITransactionService.java
@@ -1,4 +1,17 @@
 package uk.mushow.paymybuddy.services;
 
+import uk.mushow.paymybuddy.dtos.TransactionDTO;
+import uk.mushow.paymybuddy.models.Transaction;
+import uk.mushow.paymybuddy.models.Wallet;
+
+import java.math.BigDecimal;
+import java.util.List;
+
 public interface ITransactionService {
+
+    List<TransactionDTO> getTransactionsDTO(Long userId);
+
+    void transfer(Long userId, String friendEmail, BigDecimal amount, String description);
+
+    Transaction createTransaction(Wallet issuerWallet, Wallet receiverWallet, BigDecimal amount, String description);
 }
