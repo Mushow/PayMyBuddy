eec3101af0db63c33265b7f41600e71abf4da9a2
Mushow <antoine@mushow.uk>
2024-03-09 19:49:24 +0100

    Removed non used method.

--- src/main/java/uk/mushow/paymybuddy/repositories/UserRepository.java
+++ src/main/java/uk/mushow/paymybuddy/repositories/UserRepository.java
@@ -12,7 +12,6 @@ import java.util.Optional;
 @Transactional(propagation = Propagation.MANDATORY)
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
-    Optional<User> findByUsername(String username);
 
     boolean existsByEmail(String email);
 
